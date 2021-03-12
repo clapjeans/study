8d6c812ea28ffc663cd701a33651ec38e81e74f3
clapjeans <tnwls96101@daum.net>
2021-03-12 16:52:55 +0900

    codeup1014

---
+++ Java/src/codeup.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class codeup {
+
+	public static void main(String[] args) {
+
+					
+				Scanner c = new Scanner(System.in);
+		        String x = c.next();
+		        String y = c.next();
+		        
+		        		
+		      
+		        System.out.println(y+" "+x);
+		       
+
+
+			}
+			
+		
+	}
+
+
