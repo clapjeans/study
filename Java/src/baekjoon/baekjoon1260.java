e1167c46846634fbf91e197b63490ba57c2dbe50
clapjeans <tnwls96101@daum.net>
2021-07-14 23:47:44 +0900

    baekjoon1단계

---
+++ Java/src/baekjoon/baekjoon1260.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+public class baekjoon1260 {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- Java/src/pratice2.java
+++
@@ -1,8 +0,0 @@
-
-public class pratice2 {
-
-	public static void main(String[] args) {
-		System.out.println("아직도 연습중");
-	}
-
-}
