6a677cd2486ad40fa5145b881392f95fd4cc5244
clapjeans <tnwls96101@daum.net>
2021-07-29 22:43:23 +0900

    백준알고리즘

--- Java/src/baekjoon/baekjoon4673.java
+++ Java/src/baekjoon/baekjoon4673.java
@@ -1,10 +1,22 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 public class baekjoon4673 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		String n = sc.next();
+		int sum=0;
+		while(sum<10000) {
+			sum+=Integer.valueOf(n);
+					if(n.length()>1) {
+					sum+=Integer.valueOf(n.charAt(0))+Integer.valueOf(n.charAt(1));
+					}
+					System.out.println(sum);
+		}
+		
+		
 	}
 
 }
