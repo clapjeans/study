80a93a0413b3bf09b2987f5499637b8df272f65e
clapjeans <tnwls96101@daum.net>
2021-08-11 23:04:39 +0900

    백준

---
+++ Java/src/dddd.java
@@ -0,0 +1,11 @@
+
+public class dddd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] a = {74,77,87,90,95,97,93,94,98,100,85,7,19,38,14,16,21,44,46,55,63,10,29,32,34,69,70,66,42,50,51,52,47,48,49,59};
+		
+		System.out.println(a.length);
+	}
+
+}
