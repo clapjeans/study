256b9956a165a017a49de265e98a3a24b67cabde
clapjeans <tnwls96101@daum.net>
2021-08-23 22:59:37 +0900

    백준

---
+++ Java/src/baekjoon/baekjoon2750.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class baekjoon2750 {
+
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 
+		 int a = sc.nextInt();
+		 int arr[] = new int [a];
+		 for(int i =0;i<a;i++) {
+			 arr[i]=sc.nextInt();
+		 }
+		Arrays.sort(arr);
+		for(int i =0;i<a;i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
